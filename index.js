537e7b0fc17060e3b1644855c5cc7a3644abd9f8
Zanganss <76145765+Zanganss@users.noreply.github.com>
2021-01-22 21:28:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ limitawal = '20'
 cr = '*BOT INI SUDAH TERVERIFIKASI*'
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297174@s.whatsapp.net"] 
+const ownerNumber = ["6285340827717@s.whatsapp.net","6282291992581@s.whatsapp.net"] 
 /************************************/
 
        
